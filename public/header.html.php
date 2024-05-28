2e6279071748fe5ee816ec1621f4df6d9caf7a85
Maxime <maxime.loir@lepoles.org>
2024-05-28 17:01:08 +0200

    28/05/2024

---
+++ public/header.html.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Fair Book</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+
+<body>
+    <div id="content">
+    <header>
+        <h1>MyFairBook</h1>
+        <div><img src="public/assets/img/myfairbook.jpg" alt=""></div>
+         <div><i class="fa-brands fa-instagram" style="color: #ff85e4;"></i><br><br><i class="fa-brands fa-facebook" style="color: #74C0FC;"></i></div>
+    </header>
+           <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Notre histoire</a></li>
+            <li><a href="#">Le Choix du thème</a></li>
+            <li><a href="#">Le Genre du livre</a></li>
+            <li><a href="#">Idées cadeaux</a></li>
+            <li><a href="#">Nos Éditeurs</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">On parle de nous</a></li>
+            </ul>
+
+    
+
